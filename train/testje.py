38da38439e61bfd8de4865fa338e18764303f90a
maximverwilst <38004385+maximverwilst@users.noreply.github.com>
2021-06-24 22:58:29 +0200

    Delete testje.py

--- train/testje.py
+++
@@ -1,9 +0,0 @@
-import sys
-if True:
-  sys.path.append("Google Drive/Mastersthesis/TBH")
-else:
-  sys.path.append("gdrive/My Drive/Mastersthesis/TBH")
-from train import tbh_train
-import tensorflow as tf
-print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
-tbh_train.train("cifar10",32,512,1024)
