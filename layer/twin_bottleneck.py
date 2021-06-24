968851de4aec2c05d9e40a057a1c32ef7f74989f
maximverwilst <38004385+maximverwilst@users.noreply.github.com>
2021-06-24 23:11:32 +0200

    Update twin_bottleneck.py

--- layer/twin_bottleneck.py
+++ layer/twin_bottleneck.py
@@ -27,36 +27,6 @@ def hamming_split(t1):
       t1 = 1 - tf.math.abs(c1 + c2) / code_length 
       return t1
 
-@tf.function
-def build_adjacency_hamming_adapt(tensor_in):
-    """
-    Hamming-distance-based graph. It is self-connected.
-    :param tensor_in: [N D]
-    :return:
-    """
-    
-    cl = tf.shape(tensor_in)[1]
-    idxs = tf.range(cl)
-    ridxs = tf.random.shuffle(idxs)
-    rinput = tf.gather(tensor_in,ridxs,axis=1)
-
-    #rinput = hamming_split(rinput)
-
-    #t = tf.stack(tf.split(tensor_in,4,1))
-    
-    #normalized_dist = tf.map_fn(fn=hamming_split,elems=t)
-    #y=(x), y = x**2, y=1- reshape(1-x)**2
-    #normalized_dist = normalized_dist/tf.math.reduce_max(normalized_dist)
-    #normalized_dist = 1-tf.pow(normalized_dist+.0001, .5)
-    normalized_dist = tf.pow(rinput, 1.4)
-
-    #means = tf.math.reduce_mean(normalized_dist,axis=0)
-    #var = tf.math.reduce_variance(normalized_dist,axis=0)
-
-
-    return rinput#tf.stack([means,var])
-
-
 # noinspection PyAbstractClass
 class TwinBottleneck(tf.keras.layers.Layer):
     def __init__(self, bbn_dim, cbn_dim, **kwargs):
